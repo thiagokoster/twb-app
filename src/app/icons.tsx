384f460856eecc2ee1bab62e2b12b32c769ed126
koster <thiagokoster@gmail.com>
2025-05-21 19:33:56 +0200

    fix icons

--- src/app/icons.tsx
+++ src/app/icons.tsx
@@ -1,29 +1,53 @@
 export const Icons = {
 	instagram: () => (
-<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-  <path fill="currentColor" fillRule="evenodd" d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z" clipRule="evenodd"/>
-</svg>
+		<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clipPath="url(#clip0_2210_3599)">
+				<path d="M8.7 23.1372H15.3C20.8 23.1372 23 20.9372 23 15.4372V8.83721C23 3.33721 20.8 1.13721 15.3 1.13721H8.7C3.2 1.13721 1 3.33721 1 8.83721V15.4372C1 20.9372 3.2 23.1372 8.7 23.1372Z" stroke="#E9E9E9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+				<path d="M11.9999 15.9871C14.1262 15.9871 15.8499 14.2634 15.8499 12.1371C15.8499 10.0108 14.1262 8.28711 11.9999 8.28711C9.87361 8.28711 8.1499 10.0108 8.1499 12.1371C8.1499 14.2634 9.87361 15.9871 11.9999 15.9871Z" stroke="#E9E9E9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+				<path d="M18.1997 6.6374H18.2124" stroke="#E9E9E9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+			</g>
+			<defs>
+				<clipPath id="clip0_2210_3599">
+					<rect width="24" height="24" fill="white" transform="translate(0 0.137207)"/>
+				</clipPath>
+			</defs>
+		</svg>
+
 	),
 	youtube: () => (
-<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fillRule="evenodd" d="M21.7 8.037a4.26 4.26 0 0 0-.789-1.964 2.84 2.84 0 0 0-1.984-.839c-2.767-.2-6.926-.2-6.926-.2s-4.157 0-6.928.2a2.836 2.836 0 0 0-1.983.839 4.225 4.225 0 0 0-.79 1.965 30.146 30.146 0 0 0-.2 3.206v1.5a30.12 30.12 0 0 0 .2 3.206c.094.712.364 1.39.784 1.972.604.536 1.38.837 2.187.848 1.583.151 6.731.2 6.731.2s4.161 0 6.928-.2a2.844 2.844 0 0 0 1.985-.84 4.27 4.27 0 0 0 .787-1.965 30.12 30.12 0 0 0 .2-3.206v-1.516a30.672 30.672 0 0 0-.202-3.206Zm-11.692 6.554v-5.62l5.4 2.819-5.4 2.801Z" clipRule="evenodd"/>
-</svg>
+		<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M17.5 20.7372H6.5C3.2 20.7372 1 18.5372 1 15.2372V8.63721C1 5.33721 3.2 3.13721 6.5 3.13721H17.5C20.8 3.13721 23 5.33721 23 8.63721V15.2372C23 18.5372 20.8 20.7372 17.5 20.7372Z" stroke="#E9E9E9" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+			<path d="M11.3399 9.18725L14.0899 10.8372C15.0799 11.4972 15.0799 12.4872 14.0899 13.1472L11.3399 14.7972C10.2399 15.4572 9.35986 14.9072 9.35986 13.6972V10.3972C9.35986 8.96725 10.2399 8.52725 11.3399 9.18725Z" stroke="#E9E9E9" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+		</svg>
+
 	),
 	linkedin: () => (
-<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fillRule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clipRule="evenodd"/>
-  <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
-</svg>
+		<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clipPath="url(#clip0_2212_8245)">
+				<rect x="0.75" y="0.887207" width="22.5" height="22.5" rx="4.25" stroke="#E9E9E9" strokeWidth="1.5"/>
+				<path fill-rule="evenodd" clipRule="evenodd" d="M6.22211 4.58154C5.24039 4.58154 4.44434 5.37759 4.44434 6.35932C4.44434 7.34105 5.24039 8.1371 6.22211 8.1371C7.20384 8.1371 7.99989 7.34105 7.99989 6.35932C7.99989 5.37759 7.20384 4.58154 6.22211 4.58154Z" stroke="#E9E9E9" strokeWidth="1.5"/>
+				<path fill-rule="evenodd" clipRule="evenodd" d="M7.99989 10.415C7.99989 10.1389 7.77603 9.91504 7.49989 9.91504H4.94434C4.66819 9.91504 4.44434 10.1389 4.44434 10.415V19.1928C4.44434 19.469 4.66819 19.6928 4.94434 19.6928H7.49989C7.77603 19.6928 7.99989 19.469 7.99989 19.1928V10.415Z" stroke="#E9E9E9" strokeWidth="1.5"/>
+				<path fill-rule="evenodd" clipRule="evenodd" d="M15.7534 9.91504C14.2393 9.91504 13.2034 10.5469 12.7421 11.281V11.281C12.7214 11.3168 12.6667 11.3021 12.6667 11.2608V10.3307C12.6667 10.1707 12.537 10.0409 12.3769 10.0409H10.2778C10.0017 10.0409 9.77783 10.2648 9.77783 10.5409V19.1928C9.77783 19.469 10.0017 19.6928 10.2778 19.6928H12.3889C12.6651 19.6928 12.8889 19.469 12.8889 19.1928V15.0175C12.8889 13.4506 13.5001 12.4539 14.9689 12.4539C15.982 12.4539 16.4071 13.2997 16.4421 14.8078L16.4445 15.0175V19.1928C16.4445 19.469 16.6684 19.6928 16.9445 19.6928H19.0556C19.3318 19.6928 19.5556 19.469 19.5556 19.1928V14.362C19.5556 11.4837 18.8263 9.91504 15.7534 9.91504Z" stroke="#E9E9E9" strokeWidth="1.5"/>
+			</g>
+			<defs>
+				<clipPath id="clip0_2212_8245">
+					<rect width="24" height="24" fill="white" transform="translate(0 0.137207)"/>
+				</clipPath>
+			</defs>
+		</svg>
 	),
 	facebook: () => (
-<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fillRule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clipRule="evenodd"/>
-</svg>
+		<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M14.2002 9.16711V12.3571H17.0602C17.2802 12.3571 17.3902 12.5771 17.3902 12.7971L16.9502 14.8871C16.9502 14.9971 16.7302 15.1071 16.6202 15.1071H14.2002V23.1371H10.9002V15.2171H9.0302C8.8102 15.2171 8.7002 15.1071 8.7002 14.8871V12.7971C8.7002 12.5771 8.8102 12.4671 9.0302 12.4671H10.9002V8.83711C10.9002 6.96711 12.3302 5.53711 14.2002 5.53711H17.1702C17.3902 5.53711 17.5002 5.64711 17.5002 5.86711V8.50711C17.5002 8.72711 17.3902 8.83711 17.1702 8.83711H14.5302C14.3102 8.83711 14.2002 8.94711 14.2002 9.16711Z" stroke="#E9E9E9" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round"/>
+			<path d="M15.3 23.1372H8.7C3.2 23.1372 1 20.9372 1 15.4372V8.83721C1 3.33721 3.2 1.13721 8.7 1.13721H15.3C20.8 1.13721 23 3.33721 23 8.83721V15.4372C23 20.9372 20.8 23.1372 15.3 23.1372Z" stroke="#E9E9E9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+		</svg>
 	),
 	email: () => (
-<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 16v-5.5A3.5 3.5 0 0 0 7.5 7m3.5 9H4v-5.5A3.5 3.5 0 0 1 7.5 7m3.5 9v4M7.5 7H14m0 0V4h2.5M14 7v3m-3.5 6H20v-6a3 3 0 0 0-3-3m-2 9v4m-8-6.5h1"/>
-</svg>
+		<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M21.4289 3.13721H2.57171C1.62493 3.13721 0.857422 3.90473 0.857422 4.85149V19.4229C0.857422 20.3697 1.62493 21.1372 2.57171 21.1372H21.4289C22.3757 21.1372 23.1431 20.3697 23.1431 19.4229V4.85149C23.1431 3.90473 22.3757 3.13721 21.4289 3.13721Z" stroke="#E9E9E9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+			<path d="M0.857422 5.28027L10.9031 11.3218C11.2112 11.5027 11.5994 11.6017 12.0003 11.6017C12.4012 11.6017 12.7894 11.5027 13.0974 11.3218L23.1431 5.28027" stroke="#E9E9E9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+		</svg>
+
 	),
 	chevronUp: () => (
 		<svg width="24" height="13" viewBox="0 0 24 13" fill="none" xmlns="http://www.w3.org/2000/svg">
