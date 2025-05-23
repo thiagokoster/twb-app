a2c805b03b3e3c05a7220448eb31306f15666796
koster <thiagokoster@gmail.com>
2025-05-23 19:56:20 +0200

    init header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
 		<html lang="en">
 			<body
 				className={`${poppins.variable} antialiased`}>
-				<NavBar />
 				{children}
 				<Footer />
 			</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import { NavBar } from "./components/navbar";
+import { Icons } from "./icons";
+
 export default function Home() {
-  return (
-      <main className="min-h-svh bg-white">
-			Hello
-      </main>
-  );
+	return (
+		<main className="min-h-svh bg-white">
+			<div className="bg-slate-500">
+				<NavBar />
+				<div className="px-24 py-42">
+					<div className="flex flex-col gap-8">
+						<div className="text-[64px]">
+							Educação que liberta,<br/>
+							e <span className="text-twb-green">transforma vidas</span>
+						</div>
+						<button className="bg-twb-primary w-fit px-4 py-4 rounded-md flex gap-1">Como Ajudar<Icons.whatsaap /></button>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 }
