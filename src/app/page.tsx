baa7564d80e01e4e0cd0401bd2ac246c6ba2e919
koster <thiagokoster@gmail.com>
2025-07-05 11:51:21 +0200

    init map component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Carousel } from "./components/carousel";
 import { NavBar } from "./components/navbar";
 import { BlobImage } from "./components/shapes";
+import { Map } from "./components/map";
 
 export default function Home() {
 	return (
@@ -10,6 +11,7 @@ export default function Home() {
 				<Carousel />
 				
 				<BlobImage imagePath="image1.png" shapeKey="shape1" width={630} height={669} />
+				<Map />
 			</div>
 		</main>
 	);
